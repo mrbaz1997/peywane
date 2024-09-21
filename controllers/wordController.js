6890c4df6e1e5504bccdeda6ed7f0815b9f7cc9b
Îdrîs <mr.baz1997@gmail.com>
2024-09-21 17:56:42 +0330

    Refactor wordController

--- controllers/wordController.js
+++ controllers/wordController.js
@@ -1,99 +1,51 @@
 const pool = require('../config/db');
-const { is24HoursPassed } = require('../utils/timeUtils');
+// const { is24HoursPassed } = require('../utils/timeUtils');
 const new_word_indicator = -2
-const  word_limit = 40
+let time_out = true
 exports.getWordsAndQuestions = async (req, res) => {
 
     const userId = req.user.userId;
     try {
-        console.log("1. Check if user already has an active word list")
-        const result = await pool.query(
-            'SELECT * FROM user_day_words WHERE user_id = $1 ORDER BY creation_date DESC LIMIT 1',
-            [userId]
-        );
-
-        if (result.rows.length > 0) {
-            const userDayWords = result.rows[0];
-
-            console.log("Check if 24 hours have passed")
-            if (!is24HoursPassed(userDayWords.creation_date))
-            {
-                console.log("Return the current word list")
-                const wordIds = userDayWords.word_ids;
-                const words = await pool.query('SELECT * FROM words WHERE id = ANY($1)', [wordIds]);
-
-                console.log("Fetch 3 questions for each word (assuming you have 5 questions per word)")
-                const questions = await pool.query(`
-                      SELECT q.* 
-                      FROM questions q
-                      WHERE q.question_id not IN (SELECT question_id FROM scores WHERE user_id = $1 AND word_id = ANY($2))
-                      ORDER BY q.id
-                      LIMIT 3 * array_length($2, 1);
-                    `, [userId, wordIds]);
-
-                return res.json({ words: words.rows, questions: questions.rows });
-            }
-        }
-
         console.log("2. Fetch words with `ck_score = -2`")
-        const wordsWithZeroScore = await pool.query(`
+        const word = await pool.query(`
             SELECT w.*
             FROM words w
-            WHERE w.ck_score = $1
-            AND w.id NOT IN (SELECT unnest(word_ids) FROM user_day_words)
+            WHERE length(w.word) < 6
+            AND w.ck_score = $1
+            AND w.id NOT IN (SELECT word_id FROM user_day_words)
             ORDER BY RANDOM()
-            LIMIT $2;
-            `, [new_word_indicator,word_limit]);
+            LIMIT 1
+            `, [new_word_indicator]);
 
-        console.log(new_word_indicator);
-        console.log(word_limit);
-        console.log(wordsWithZeroScore);
-        console.log(wordsWithZeroScore.rows);
-        const words = wordsWithZeroScore.rows;
-
-        console.log("3. If fewer than 10 words are found, add words with other `ck_score` values")
-        if (words.length < 10)
-        {
-            const additionalWords = await pool.query(`
-                SELECT w.*
-                FROM words w
-                WHERE w.ck_score > 1
-                AND w.id NOT IN (SELECT unnest(word_ids) FROM user_day_words)
-                AND w.id NOT IN ($1)  -- Exclude already fetched words
-                ORDER BY RANDOM()
-                LIMIT $2;`,
-                [words.map(word => word.id), word_limit - words.length]);
-
-            words.push(...additionalWords.rows);
+        console.log("3. Save the word IDs in the `user_day_words` table")
+        if(time_out){
+            console.log("Updating table");
+            await pool.query(
+                'UPDATE user_day_words set word_id = $2 WHERE user_id= $1',
+                [userId, word.rows[0].id]
+            );
+        }
+        else{
+            console.log("Inserting table");
+            await pool.query(
+                'INSERT INTO user_day_words (user_id, word_id) VALUES ($1, $2)',
+                [userId, word.rows[0].id]
+            );
         }
 
-        console.log("If no words are found, handle the case")
-        if (words.length < 10) {
-            return res.status(404).json({ error: 'Not enough words available' });
-        }
-
-        console.log("Trim words to a maximum of new_word_indicator")
-        const finalWords = words.slice(0, word_limit);
-        let wordIds = finalWords.map((word) => word.id);
-
-        console.log("4. Save the word IDs in the `user_day_words` table")
-        await pool.query(
-            'INSERT INTO user_day_words (user_id, word_ids) VALUES ($1, $2)',
-            [userId, wordIds]
-        );
-
-        console.log("5. Fetch 5 questions for each word")
+        console.log("4. Fetch 3 questions for each word")
         const questions = await pool.query(`
-            SELECT q.* 
+            SELECT q.passage
             FROM questions q
             WHERE q.language_id = 1
             AND q.question_id not IN (SELECT question_id FROM scores WHERE user_id = $1 AND word_id = ANY($2))
             ORDER BY q.id
-            LIMIT 5 * array_length($2, 1);
-            `, [userId, wordIds]);
+            LIMIT 3;
+            `, [userId, word.id]);
 
-        console.log("6. Return the words along with their questions")
-        res.json({ words: finalWords, questions: questions.rows });
+        console.log("5. Return the words along with their questions")
+        console.log(word.rows[0].word)
+        res.json({ word: word.rows[0].word, questions: questions.rows });
     } catch (err) {
         res.status(500).json({ error: err.message });
     }

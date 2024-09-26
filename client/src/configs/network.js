9540d13d7d862157cb78bb84a14803c819ea620b
Îdrîs <mr.baz1997@gmail.com>
2024-09-26 15:28:50 +0330

    Remove local Proxy in client

--- client/src/configs/network.js
+++ client/src/configs/network.js
@@ -1,1 +1,1 @@
-export const BACKEND_BASE_URL = "http://localhost:5000";
+export const BACKEND_BASE_URL = "https://peywane.xyz";
--- controllers/authController.js
+++ controllers/authController.js
@@ -31,7 +31,7 @@ exports.signin = async (req, res) => {
         }
 
         const token = jwt.sign({ userId: user.rows[0].id }, process.env.JWT_SECRET, {
-            expiresIn: '1h',
+            expiresIn: '24h',
         });
         res.json({ token });
     } catch (err) {

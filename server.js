e58eabd50ef8ab236f541151a94f730108b1b0f5
Îdrîs <mr.baz1997@gmail.com>
2024-09-29 21:27:05 +0330

    change routing type and address

--- server.js
+++ server.js
@@ -22,10 +22,12 @@ app.use('/auth', authRoutes);
 app.use('/words', wordRoutes);
 
 // Handle all other routes and send back the React index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
+// Serve the React app for any other route
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
